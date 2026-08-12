baef1ab7494ba3d7879f1f13a86df10401ed725b
Rajisep12 <43602173+Rajisep12@users.noreply.github.com>
2026-08-12 15:54:38 +0530

    12-6-26-version2-config

--- salaryproject/frontend/config.js
+++ salaryproject/frontend/config.js
@@ -1,5 +1,8 @@
 // export const BASE_URL = 'https://ajmivault.com/api';  // No trailing '/'
 // export const IMAGE_URL = 'https://ajmivault.com';  // No trailing '/'
 
-export const BASE_URL = 'http://localhost:8000/api'
-export const IMAGE_URL = 'http://localhost:8000'
+//export const BASE_URL = 'http://localhost:8000/api'
+//export const IMAGE_URL = 'http://localhost:8000'
+
+export const BASE_URL = 'https://hayatigarments.onrender.com/api'
+export const IMAGE_URL = 'https://hayatigarments.onrender.com'
